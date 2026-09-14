d4f0446b8daab155bcbc6dc8c16ede0d7ce92374
sttesta3 <sttesta@fi.uba.ar>
2026-09-13 21:11:35 -0300

    Al detener rabbitmq se elevaron los errores (pika.exceptions.AMQPConnectionError,pika.exceptions.AMQPChannelError, pika.exceptions.StreamLostError), se crea un manejo extra para estos

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -3,14 +3,21 @@ import random
 import string
 from .middleware import MessageMiddlewareCloseError, MessageMiddlewareDisconnectedError, MessageMiddlewareMessageError, MessageMiddlewareQueue, MessageMiddlewareExchange
 
+AMQP_NETWORK_EXCEPTIONS = (pika.exceptions.AMQPConnectionError,pika.exceptions.AMQPChannelError, pika.exceptions.StreamLostError)
+
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
         # https://www.rabbitmq.com/tutorials/tutorial-one-python
         # Sin argumentos crea una cola clasica (en el ejemplo crea una quorum)
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
-        self.channel = self.connection.channel()
-        self.queue = self.channel.queue_declare(queue=queue_name)
+        try:
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+            self.channel = self.connection.channel()
+            self.queue = self.channel.queue_declare(queue=queue_name)
+        except AMQP_NETWORK_EXCEPTIONS:
+            raise MessageMiddlewareDisconnectedError
+        except Exception:
+            raise MessageMiddlewareMessageError()
 
     def start_consuming(self, on_message_callback):
         def callback(ch, method, properties, body):
@@ -25,6 +32,8 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             self.channel.start_consuming()
         except pika.exceptions.ChannelClosed:
             raise MessageMiddlewareDisconnectedError()
+        except AMQP_NETWORK_EXCEPTIONS:
+            raise MessageMiddlewareDisconnectedError
         except Exception:
             # pika.exceptions.ReentrancyError cae bajo esta Excepcion
             raise MessageMiddlewareMessageError()
@@ -32,6 +41,8 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
     def stop_consuming(self):
         try:
             self.channel.stop_consuming()
+        except AMQP_NETWORK_EXCEPTIONS:
+            raise MessageMiddlewareDisconnectedError
         except Exception:
             raise MessageMiddlewareDisconnectedError()
 
@@ -44,6 +55,8 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             )
         except pika.exceptions.ChannelClosed(): # https://pika.readthedocs.io/en/stable/modules/exceptions.html
             raise MessageMiddlewareDisconnectedError()
+        except AMQP_NETWORK_EXCEPTIONS:
+            raise MessageMiddlewareDisconnectedError
         except Exception as e:
             print("Error ", e)
             raise MessageMiddlewareMessageError()
@@ -52,25 +65,33 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         # No debe revisarse connection.is_open ya que close lo revisa y raise ConnectionWrongStateError
         try:
             self.connection.close()
+        except AMQP_NETWORK_EXCEPTIONS:
+            raise MessageMiddlewareDisconnectedError
         except Exception as e:
             print(f"Error {e}")
             raise MessageMiddlewareCloseError()
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def __init__(self, host, exchange_name, routing_keys):
-        print(f"exchange {exchange_name}\nrk: {routing_keys}")
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
-        self.channel = self.connection.channel()
+        try:
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+            self.channel = self.connection.channel()
 
-        # Exchange durable porque tiene que compartirse entre todos 
-        self.exchange_name = exchange_name
-        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct', durable=True)
+            # Exchange durable porque tiene que compartirse entre todos 
+            self.exchange_name = exchange_name
+            self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct', durable=True)
 
-        # Cola exclusiva bindeada a todas las llaves de ruteo 
-        self.queue = self.channel.queue_declare(queue='', exclusive=True)
-        self.routing_keys = routing_keys
-        for key in self.routing_keys: 
-            self.channel.queue_bind(exchange=exchange_name, queue=self.queue.method.queue, routing_key=key)
+            # Cola exclusiva bindeada a todas las llaves de ruteo 
+            self.queue = self.channel.queue_declare(queue='', exclusive=True)
+            self.routing_keys = routing_keys
+            for key in self.routing_keys: 
+                self.channel.queue_bind(exchange=exchange_name, queue=self.queue.method.queue, routing_key=key)
+
+        except AMQP_NETWORK_EXCEPTIONS:
+            raise MessageMiddlewareDisconnectedError
+        except Exception:
+            raise MessageMiddlewareMessageError()
+
 
     def start_consuming(self, on_message_callback):
         def callback(ch, method, properties, body):
@@ -86,6 +107,8 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
             self.channel.start_consuming()
         except pika.exceptions.ChannelClosed:
             raise MessageMiddlewareDisconnectedError()
+        except AMQP_NETWORK_EXCEPTIONS:
+            raise MessageMiddlewareDisconnectedError
         except Exception:
             # pika.exceptions.ReentrancyError cae bajo esta Excepcion
             raise MessageMiddlewareMessageError()
@@ -93,6 +116,8 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def stop_consuming(self):
         try:
             self.channel.stop_consuming()
+        except AMQP_NETWORK_EXCEPTIONS:
+            raise MessageMiddlewareDisconnectedError
         except Exception:
             raise MessageMiddlewareDisconnectedError()
 
@@ -102,6 +127,8 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
                 self.channel.basic_publish(exchange=self.exchange_name, routing_key=key, body=message)
         except pika.exceptions.ChannelClosed(): # https://pika.readthedocs.io/en/stable/modules/exceptions.html
             raise MessageMiddlewareDisconnectedError()
+        except AMQP_NETWORK_EXCEPTIONS:
+            raise MessageMiddlewareDisconnectedError
         except Exception as e:
             print("Error ", e)
             raise MessageMiddlewareMessageError()
@@ -110,6 +137,8 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
         # No debe revisarse connection.is_open ya que close lo revisa y raise ConnectionWrongStateError
         try:
             self.connection.close()
+        except AMQP_NETWORK_EXCEPTIONS:
+            raise MessageMiddlewareDisconnectedError
         except Exception as e:
             print(f"Error {e}")
             raise MessageMiddlewareCloseError()
